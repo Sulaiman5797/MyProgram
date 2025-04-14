548c01754aafc3380558adb9979c9ed4fa30a3a8
Sulaiman5797 <mdsulaiman5797@gmail.com>
2025-04-14 06:12:50 -0700

    main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+myName= 'Sulaiman'
+myAge= 27
+is_student= False
+print (myName, type(myName))
+print (myAge, type (myAge))
+print (is_student, type (is_student))
+print (myAge * 10)
+print (myAge - 10)
+print (myAge + 10)
+print (myAge / 10)
+age = myAge>10
+print (age)
+age = myAge<10
+print (age)
+age = myAge!=10
+print (age)
+x= [10, 27, 6, 5, 9, 0]
+y= x
+z= 6
+print (x is y)
+print (x is not y)
+print (z in x)
+print (not (z in x))
